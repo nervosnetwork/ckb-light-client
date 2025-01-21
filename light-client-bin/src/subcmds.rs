d71505484509d212e392c01f19eed5f816af1319
officeyutong <yt.xyxx@gmail.com>
2025-01-21 14:05:29 +0800

    fmt

--- light-client-bin/src/subcmds.rs
+++ light-client-bin/src/subcmds.rs
@@ -3,7 +3,8 @@ use std::sync::{Arc, RwLock};
 use ckb_async_runtime::new_global_runtime;
 use ckb_chain_spec::ChainSpec;
 use ckb_network::{
-    network::TransportType, tokio, CKBProtocol, CKBProtocolHandler, Flags, NetworkService, NetworkState, SupportProtocols
+    network::TransportType, tokio, CKBProtocol, CKBProtocolHandler, Flags, NetworkService,
+    NetworkState, SupportProtocols,
 };
 use ckb_resource::Resource;
 use ckb_stop_handler::{broadcast_exit_signals, wait_all_ckb_services_exit};
@@ -128,7 +129,7 @@ impl RunConfig {
                 clap::crate_version!().to_owned(),
                 Flags::DISCOVERY,
             ),
-            TransportType::Tcp
+            TransportType::Tcp,
         )
         .start(&handle)
         .map_err(|err| {
