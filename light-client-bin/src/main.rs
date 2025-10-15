8e043c6ae20c8ac480c228ffa0093a0631db9017
Eval EXEC <execvy@gmail.com>
2025-10-15 15:31:33 +0800

    Use tikv-jemallocator on non msvc platform

--- light-client-bin/src/main.rs
+++ light-client-bin/src/main.rs
@@ -9,6 +9,10 @@ mod tests;
 use cli::AppConfig;
 use env_logger::{Builder, Env, Target};
 
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+
 fn main() -> anyhow::Result<()> {
     let mut builder = Builder::from_env(Env::default());
     builder.target(Target::Stdout);
