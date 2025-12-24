ef01a1419af31c5ba8e41f7c7cee7c6ca4c00fba
officeyutong <yt.xyxx@gmail.com>
2025-12-24 16:08:39 +0800

    update banner

--- wasm/light-client-js/esbuild.config.mjs
+++ wasm/light-client-js/esbuild.config.mjs
@@ -31,6 +31,6 @@ await esbuild.build({
         "stream"
     ],
     banner: {
-        js: "import { createRequire } from 'module'; const require = createRequire(import.meta.url);"
+        js: "import { createRequire } from 'module'; import { fileURLToPath } from 'url'; import { dirname } from 'path'; const require = createRequire(import.meta.url); const __filename = fileURLToPath(import.meta.url); const __dirname = dirname(__filename);"
     }
 })
